4b1524e77d65701b111489a55a3f83f54ae95283
Mike Suhan <you@example.com>
2019-06-18 07:17:24 +0300

    Added QUIT and HELP commands

--- cli.py
+++ cli.py
@@ -2,13 +2,23 @@ from thesaurus import Thesaurus
 
 thes = Thesaurus()
 
+print('Type HELP to see a list of commands.\n')
+
 while True:
     query = input('Enter a word to look it up in the thesaurus:\n')
 
-    if len(query.strip().split()) > 1:
+    if query.strip() == 'HELP':
+        print('QUIT'.ljust(25), 'Exits the program.')
+        print('REQUIRE <word list(s)>'.ljust(25), 'Shows only results from these specific word list(s).')
+        print()
+
+    elif query.strip() == 'QUIT':
+        quit()
+
+    elif len(query.strip().split()) > 1:
 
         # change required word list(s)
-        if query.lower().startswith('require'):
+        if query.lower().startswith('REQUIRE'):
             require = [item.strip() for item in query.split()[1:]]
             thes = Thesaurus(required=require)
             print('\nOnly matches in the following list{} will be shown:\n{}\n'.format(
