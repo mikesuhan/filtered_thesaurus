b757e886784cd4a710ee505481e7e12d8df51225
Mike Suhan <you@example.com>
2019-06-19 17:20:39 +0300

    Added RESET SETTINGS option

--- cli.py
+++ cli.py
@@ -10,13 +10,17 @@ while True:
     if query.strip() == 'HELP':
         print('QUIT'.ljust(25), 'Exits the program.')
         print('REQUIRE <word list(s)>'.ljust(25), 'Shows only results from these specific word list(s).')
-        print('REQUIRE ANY').ljus(25), 'Does not show words that are not in a word list.'
+        print('REQUIRE ANY'.ljust(25), 'Does not show words that are not in a word list.')
+        print('RESET SETTINGS'.ljust(25), 'Resets to default settings.')
         print()
         print('To add a new word list, create a txt file in the filters directory with each word on a new line. Then restart the program.')
 
-
     elif query.strip() == 'QUIT':
         quit()
+
+    elif query.strip().lower() == 'reset settings':
+        thes = Thesaurus()
+        print('Settings have been reset.')
 
     elif query.strip() == 'require any':
         thes = Thesaurus(must_match=True)
